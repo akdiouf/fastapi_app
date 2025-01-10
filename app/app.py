ce8883eb557bd3acb90d9fffd0e4a6d951725dd8
Abdou Karim DIouf <61091794+akdiouf@users.noreply.github.com>
2025-01-10 15:01:01 +0000

    firstpush

--- app/app.py
+++ app/app.py
@@ -1,24 +1,20 @@
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 from mangum import Mangum
 from fastapi.responses import JSONResponse
 import uvicorn
-from typing import Union
-
 app = FastAPI()
-handler = Mangum(app)
-
-@app.get("/")
+router = APIRouter()
+@router.get("/")
 def read_root():
-   return {"Welcome to": "My first FastAPI depolyment using Docker image"}
-
-@app.get("/{text}")
+    return {"Welcome to": "My first FastAPI deployment using Docker image"}
+@router.get("/{text}")
 def read_item(text: str):
-   return JSONResponse({"result": text})
-
-@app.get("/items/{item_id}")
+    return JSONResponse({"result": text})
+@router.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
-   return JSONResponse({"item_id": item_id, "q": q})
-
+    return JSONResponse({"item_id": item_id, "q": q})
+app.include_router(router)
+handler = Mangum(app)
 if __name__ == "__main__":
-   uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
